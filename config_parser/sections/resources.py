56e956aa8525fd32676f261bc4969c058aaeacb1
elmarhaussmann <elmar@riseml.com>
2018-04-10 18:02:55 +0800

    Add TPU support

--- config_parser/sections/resources.py
+++ config_parser/sections/resources.py
@@ -1,7 +1,8 @@
 from .base import SectionBase
 
 class ResourcesSection(SectionBase):
-    def __init__(self, cpus=None, gpus=None, mem=None):
+    def __init__(self, cpus=None, mem=None, tpus=0, gpus=0):
         self.cpus = cpus
         self.mem = mem
         self.gpus = gpus
+        self.tpus = tpus
